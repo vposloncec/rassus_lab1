4b4069fe67132e9ea22fb98fef8ff75eed530136
Viktor Posloncec <viktor.posloncec@gmail.com>
2021-11-10 04:28:50 +0100

    Fix seg fault and refactoring for easier reading of client

--- klijent/main.go
+++ klijent/main.go
@@ -9,7 +9,6 @@ import (
 	pb "github.com/vposloncec/rassus_lab1/klijent/proto"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
-	"log"
 	"math/rand"
 	"net"
 	"os"
@@ -21,10 +20,13 @@ type grpcServer struct {
 	pb.UnimplementedSensorServer
 	currTime time.Time
 	readings []*pb.Reading
+	logger   *zap.SugaredLogger
 }
 
 func (s *grpcServer) GetReading(context.Context, *pb.GetReadingRequest) (*pb.Reading, error) {
-	return makeReading(s.currTime, s.readings), nil
+	reading := makeReading(s.logger, s.currTime, s.readings)
+	s.logger.Infow("Got GRPC call for getReading, responding with ", "reading", reading)
+	return reading, nil
 }
 
 type RegisterRequest struct {
@@ -41,32 +43,39 @@ type RegisterResponse struct {
 	Id        int
 }
 
+const SERVER = "http://127.0.0.1:3000"
+
 func main() {
+	// Initialization
 	start := time.Now()
 	rawLog, _ := zap.NewDevelopment()
 	readings := readCsvFile()
 	log := rawLog.Sugar()
 	client := resty.New()
+	calibrationEnabled := true
+	client.SetBaseURL(SERVER)
 	rand.Seed(start.UnixNano())
-	//client.Debug = true
-	// Set up a connection to the server.
-	registerReq := RegisterRequest{
+
+	SensorInfo := RegisterRequest{
 		Latitude:  15.87 + rand.Float64()*(16-15.86),
 		Longitude: 45.75 + rand.Float64()*(45.85-45.75),
 		Ip:        "127.0.0.1",
 		Port:      10000 + rand.Intn(10000),
 	}
 
-	jsonReq, err := json.Marshal(registerReq)
-	log.Infof("%v", string(jsonReq))
+	jsonReq, err := json.Marshal(SensorInfo)
+	log.Infof("Sensor initialized with: %v", string(jsonReq))
 	if err != nil {
 		log.Fatal("could not marshall register req")
 	}
+
+	// Register on server
+	log.Debugf("Using server %v for registration", SERVER)
 	r, err := client.R().
 		SetHeader("Content-Type", "application/json").
 		SetBody(jsonReq).
 		SetResult(&RegisterResponse{}).
-		Post("http://127.0.0.1:3000/register")
+		Post("/register")
 	if err != nil {
 		log.Fatal("could not connect to centralized server: ", err)
 	}
@@ -76,54 +85,85 @@ func main() {
 			string(r.Body()))
 	} else {
 		log.Errorf("Could not register, response status: %v", r.Status())
+		os.Exit(1)
 	}
 
-	go startGrpcServer(start, strconv.Itoa(registerReq.Port))
+	// Start accepting grpc reading requests
+	go startGrpcServer(log, start, strconv.Itoa(SensorInfo.Port))
 
-	//Get neighbour
-	myId := r.Result().(*RegisterResponse).Id
+	// Get neighbour
+	log.Debugf("Fetching nearest neighbour from server %v", SERVER)
+	myId := strconv.Itoa(r.Result().(*RegisterResponse).Id)
 	r, err = client.R().
 		SetResult(&RegisterResponse{}).
-		Get("http://127.0.0.1:3000/sensors/" + strconv.Itoa(myId) + "/nearest")
+		Get("/sensors/" + myId + "/nearest")
 
 	neighbour := r.Result().(*RegisterResponse)
-	if neighbour == nil {
-		log.Infof("No neighbour! skipping grpc calls...")
+	if r.IsError() || neighbour == nil {
+		log.Error("nearest neighbour request error: ", r.Error())
+		log.Infof("No neighbour! Grpc calls will be skipped, no calibration.")
+		calibrationEnabled = false
+	} else {
+		log.Infof("My neighbour has id %v, calling him on ip: %v, port: %v", neighbour.Id, neighbour.Ip, neighbour.Port)
 	}
-	log.Infof("My neighbour has id %v, calling him on ip: %v, port: %v", neighbour.Id, neighbour.Ip, neighbour.Port)
-
-	neighbourAddr := neighbour.Ip + ":" + strconv.Itoa(neighbour.Port)
-	currentReading := makeReading(start, readings)
-	log.Info("Current reading: ", currentReading)
 
 	// Initialize grpc client
+	neighbourAddr := neighbour.Ip + ":" + strconv.Itoa(neighbour.Port)
 	conn, err := grpc.Dial(neighbourAddr, grpc.WithInsecure())
-	c := pb.NewSensorClient(conn)
 	if err != nil {
-		log.Fatalf("grpc did not connect: %v", err)
-	}
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-	neighbourReading, err := c.GetReading(ctx, &pb.GetReadingRequest{})
-	if err != nil {
-		log.Errorf("error while contacting neighbour: %v", err)
+		log.Errorf("grpc could not connect: %v", err)
+		calibrationEnabled = false
 	} else {
-		log.Infof("Got reading from neighbour %v", neighbourReading)
+		defer conn.Close()
 	}
-	//if err != nil {
-	defer conn.Close()
+	c := pb.NewSensorClient(conn)
 
-	//// Contact the server and print out its response.
-	//ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	//defer cancel()
-	//r, err := c.SayHello(ctx, &pb.HelloRequest{Name: *name})
-	//if err != nil {
-	//	log.Fatalf("could not greet: %v", err)
-	//}
-	//log.Printf("Greeting: %s", r.GetMessage())
+	// Reading loop
+	for {
+		reading := makeReading(log, start, readings)
+		log.Info("Original reading: ", reading)
+
+		if calibrationEnabled {
+			ctx, _ := context.WithTimeout(context.Background(), time.Second)
+			neighbourReading, err := c.GetReading(ctx, &pb.GetReadingRequest{})
+			if err != nil {
+				log.Errorf("error while contacting neighbour: %v, skipping calibration", err)
+				calibrationEnabled = false
+			} else {
+				log.Infof("Got reading from neighbour %v", neighbourReading)
+				reading = &pb.Reading{
+					Temperature: mergeReadings(reading.Temperature, neighbourReading.Temperature),
+					Pressure:    mergeReadings(reading.Pressure, neighbourReading.Pressure),
+					Humidity:    mergeReadings(reading.Humidity, neighbourReading.Humidity),
+					Co:          mergeReadings(reading.Co, neighbourReading.Co),
+					No2:         mergeReadings(reading.No2, neighbourReading.No2),
+					So2:         mergeReadings(reading.So2, neighbourReading.So2),
+				}
+			}
+		}
+
+		serialized, _ := json.Marshal(reading)
+		log.Debugf("sending reading %v to server", string(serialized))
+
+		// Send reading to server
+		r, err := client.R().
+			SetHeader("Content-Type", "application/json").
+			SetBody(serialized).
+			Post("/sensors/" + myId + "/readings")
+		if r.IsSuccess() {
+			log.Infof("Successfully sent reading, return status %v; server response: %v", r.Status(),
+				string(r.Body()))
+		} else {
+			log.Errorf("Error sending reading, response status: %v", r.Status())
+			log.Error(err)
+		}
+
+		// Sleep for 3 to 8 seconds
+		time.Sleep(time.Duration(3+rand.Intn(5)) * time.Second)
+	}
 }
 
-func startGrpcServer(startTime time.Time, port string) {
+func startGrpcServer(log *zap.SugaredLogger, startTime time.Time, port string) {
 	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%v", port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
@@ -132,36 +172,52 @@ func startGrpcServer(startTime time.Time, port string) {
 	pb.RegisterSensorServer(s, &grpcServer{
 		currTime: startTime,
 		readings: readCsvFile(),
+		logger:   log.Named("GrpcServer"),
 	})
-	log.Printf("server listening at %v", lis.Addr())
+	log.Infof("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
 
-func makeReading(current time.Time, readings []*pb.Reading) *pb.Reading {
+func makeReading(log *zap.SugaredLogger, current time.Time, readings []*pb.Reading) *pb.Reading {
 	elapsed := time.Since(current)
-	return readings[(int(elapsed.Seconds()) % 100)]
+	pickedReading := int(elapsed.Seconds()) % 100
+	// log.Debugf("Time running: %v, picked reading number %v", elapsed, pickedReading)
+	return readings[pickedReading]
 }
 
 func readCsvFile() []*pb.Reading {
 	csvFile, _ := os.Open("readings.csv")
 	csvLines, _ := csv.NewReader(csvFile).ReadAll()
 	var readings []*pb.Reading
-	for _, line := range csvLines {
+	for _, line := range csvLines[1:] {
 		temp, _ := strconv.ParseFloat(line[0], 32)
 		pressure, _ := strconv.ParseFloat(line[1], 32)
 		h, _ := strconv.ParseFloat(line[2], 32)
 		co, _ := strconv.ParseFloat(line[3], 32)
-		so2, _ := strconv.ParseFloat(line[4], 32)
+		no2, _ := strconv.ParseFloat(line[4], 32)
+		so2, _ := strconv.ParseFloat(line[5], 32)
 		reading := &pb.Reading{
 			Temperature: float32(temp),
 			Pressure:    float32(pressure),
 			Humidity:    float32(h),
 			Co:          float32(co),
+			No2:         float32(no2),
 			So2:         float32(so2),
 		}
 		readings = append(readings, reading)
 	}
 	return readings
+}
+
+func mergeReadings(r1, r2 float32) float32 {
+	var defaultValue float32
+	if r2 == defaultValue {
+		return r1
+	} else if r1 == defaultValue {
+		return r2
+	}
+
+	return (r1 + r2) / 2
 }
